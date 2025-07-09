35d2bc9befd61224fa1455a6b167a7813f0500f0
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-07-09 15:13:45 +0530

    updated server.js to use production settings for CORS and static file serving

--- backend/server.js
+++ backend/server.js
@@ -60,15 +60,15 @@ app.use('/api/actionlogs', actionLogRoutes);
 
 // Serve frontend (if applicable for deployment)
 // For local development, this part is often commented out or handled differently
-//if (process.env.NODE_ENV === 'production') {
-  //  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    //app.get('*', (req, res) =>
-      //  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'))
-    //);
-//} else {
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'))
+    );
+} else {
     app.get('/', (req, res) => res.send('Please set to production'));
-//}
+}
 
 // Error handling middleware (should be last middleware)
 app.use(errorHandler);
